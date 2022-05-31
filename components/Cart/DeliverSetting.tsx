e94fd80fb9e640de48b869944137bec9ea3ede94
M.W <auddnjs2008@naver.com>
2022-05-31 09:57:55 +0900

    카트 페이지에서 베송지 추가 기능 추가

--- components/Cart/DeliverSetting.tsx
+++ components/Cart/DeliverSetting.tsx
@@ -1,5 +1,7 @@
 import AddressFix from '@components/MyPage/AddressFix';
+import AddressWindow from '@components/MyPage/AddressWindow';
 import { cls } from '@libs/cls';
+import execDaumPostcode from '@libs/execDaumPostcode';
 import useMutate from '@libs/useMutate';
 import { RootState } from '@modules/index';
 import { ICartInfoResult } from 'pages/cart';
@@ -20,6 +22,9 @@ const DeliverSetting = ({ setAddressFixBtn, likeAddressMutate }: IDeliverSetting
     const { data, mutate } = useSWR<IGetAddressResult>(`http://prod.hiimpedro.site:9000/app/users/${user.userIdx}/Address`);
     const [likeClickInfo, setLikeClickInfo] = useState<IAddressInfo | null>(null);
     const [fixAddressInfo, setFixAddressInfo] = useState<IAddressInfo | null>(null);
+    const [addAddressInfo, setAddAddressInfo] = useState("");
+
+
     const [likemutate] = useMutate(`http://prod.hiimpedro.site:9000/app/users/${user.userIdx}/Address/${likeClickInfo?.address_idx}/like`, true);
 
 
@@ -40,6 +45,10 @@ const DeliverSetting = ({ setAddressFixBtn, likeAddressMutate }: IDeliverSetting
             setLikeClickInfo({ ...data.result[+like], idx: +like });
         }
 
+    }
+
+    const onAddAddressClick = () => {
+        execDaumPostcode(setAddAddressInfo);
     }
 
     useEffect(() => {
@@ -72,54 +81,57 @@ const DeliverSetting = ({ setAddressFixBtn, likeAddressMutate }: IDeliverSetting
         fixAddressInfo ?
             <AddressFix fixAddressInfo={fixAddressInfo} setFixAddressInfo={setFixAddressInfo} addressListMutate={mutate} addressMutate={likeAddressMutate} />
             :
-            <div className="fixed  top-1/2 left-1/2  -translate-x-1/2 -translate-y-1/2  bg-white shadow-xl z-30">
-                <button onClick={() => setAddressFixBtn(false)} className="absolute top-3 right-5">
-                    <svg xmlns="http://www.w3.org/2000/svg" className="h-7 w-7" viewBox="0 0 20 20" fill="currentColor">
-                        <path fillRule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clipRule="evenodd" />
-                    </svg>
-                </button>
-                <div className=" p-10">
-                    <header className="flex space-x-3 items-center">
-                        <h1 className="text-3xl" >배송지</h1>
-                        <h3 className="text-sm text-gray-600">배송지에 따라 상품정보 및 배송유형이 달라질 수 있습니다.</h3>
-                    </header>
-                    <main className="mt-5">
-                        <ul onClick={onFixOrLikeClick} className="h-[50vh] overflow-auto">
-                            <li className="grid grid-cols-[1.5fr_8fr_1.5fr] justify-items-center p-3 border-t-2 border-b-[1px] border-black">
-                                <div>선택</div>
-                                <div>배송 정보</div>
-                                <div>수정</div>
-                            </li>
-                            {data?.result.map((item, index) =>
-                                <li className="grid grid-cols-[1.5fr_8fr_1.5fr] justify-items-center py-5 border-b-[1px]">
-                                    <button data-like={index} className={cls(item.is_like === "Y" ? "bg-purple-800" : "", "mr-2 border-2 rounded-full h-8 w-8 p-1")}>
-                                        <svg xmlns="http://www.w3.org/2000/svg" className={cls(item.is_like === "Y" ? "text-white" : "", "h-5 w-5")} fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
-                                            <path strokeLinecap="round" strokeLinejoin="round" d="M5 13l4 4L19 7" />
-                                        </svg>
-                                    </button>
-                                    <div className="justify-self-start space-y-2">
-                                        <div>{item.address_main + " " + item.address_desc} </div>
-                                        {item.recevied_name ?
-                                            <div className="space-x-2 text-sm text-gray-600">
-                                                <span>{item.recevied_name}</span>
-                                                <span>|</span>
-                                                <span>{item.recevied_phone.substring(0, 3) + "-" + item.recevied_phone.substring(3, 7) +
-                                                    "-" + item.recevied_phone.substring(7)}</span>
-                                            </div>
-                                            :
-                                            null}
-                                    </div>
-                                    <button data-id={index}>
-                                        <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
-                                            <path strokeLinecap="round" strokeLinejoin="round" d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z" />
-                                        </svg>
-                                    </button>
-                                </li>)}
-                        </ul>
-                    </main>
+            addAddressInfo ?
+                <AddressWindow setAddAddressInfo={setAddAddressInfo} info={addAddressInfo} addressMutate={mutate} />
+                :
+                <div className="fixed  top-1/2 left-1/2  -translate-x-1/2 -translate-y-1/2  bg-white shadow-xl z-30">
+                    <button onClick={() => setAddressFixBtn(false)} className="absolute top-3 right-5">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-7 w-7" viewBox="0 0 20 20" fill="currentColor">
+                            <path fillRule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clipRule="evenodd" />
+                        </svg>
+                    </button>
+                    <div className=" p-10">
+                        <header className="flex space-x-3 items-center">
+                            <h1 className="text-3xl" >배송지</h1>
+                            <h3 className="text-sm text-gray-600">배송지에 따라 상품정보 및 배송유형이 달라질 수 있습니다.</h3>
+                        </header>
+                        <main className="mt-5">
+                            <ul onClick={onFixOrLikeClick} className="h-[50vh] overflow-auto">
+                                <li className="grid grid-cols-[1.5fr_8fr_1.5fr] justify-items-center p-3 border-t-2 border-b-[1px] border-black">
+                                    <div>선택</div>
+                                    <div>배송 정보</div>
+                                    <div>수정</div>
+                                </li>
+                                {data?.result.map((item, index) =>
+                                    <li className="grid grid-cols-[1.5fr_8fr_1.5fr] justify-items-center py-5 border-b-[1px]">
+                                        <button data-like={index} className={cls(item.is_like === "Y" ? "bg-purple-800" : "", "mr-2 border-2 rounded-full h-8 w-8 p-1")}>
+                                            <svg xmlns="http://www.w3.org/2000/svg" className={cls(item.is_like === "Y" ? "text-white" : "", "h-5 w-5")} fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
+                                                <path strokeLinecap="round" strokeLinejoin="round" d="M5 13l4 4L19 7" />
+                                            </svg>
+                                        </button>
+                                        <div className="justify-self-start space-y-2">
+                                            <div>{item.address_main + " " + item.address_desc} </div>
+                                            {item.recevied_name ?
+                                                <div className="space-x-2 text-sm text-gray-600">
+                                                    <span>{item.recevied_name}</span>
+                                                    <span>|</span>
+                                                    <span>{item.recevied_phone.substring(0, 3) + "-" + item.recevied_phone.substring(3, 7) +
+                                                        "-" + item.recevied_phone.substring(7)}</span>
+                                                </div>
+                                                :
+                                                null}
+                                        </div>
+                                        <button data-id={index}>
+                                            <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
+                                                <path strokeLinecap="round" strokeLinejoin="round" d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z" />
+                                            </svg>
+                                        </button>
+                                    </li>)}
+                            </ul>
+                        </main>
+                    </div>
+                    <button onClick={onAddAddressClick} className="border-t-[1px] w-full  p-5">+ 새 배송지 추가</button>
                 </div>
-                <button className="border-t-[1px] w-full  p-5">+ 새 배송지 추가</button>
-            </div>
 
     )
 }
